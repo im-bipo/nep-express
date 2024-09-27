3b8972a128afdb653daa00fcbe76e890ddef349d
im-bipo <bipinkhatri.ram@gmail.com>
2024-09-27 17:57:31 +0545

    User Profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,27 +1,24 @@
-import {
-  getKindeServerSession,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/server";
+import { getUserData } from "@/actions/user";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
-
 const Profile = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-
   if (!user) {
-    return <div>User not found</div>;
+    console.log("User not found");
+    return <div>You are not logged in.</div>;
   }
+  // Check if the user exists in the database
+  const userData = await getUserData({ id: user.id });
+  if (userData?.role == "GUEST") return <h1>Guest Profile</h1>;
 
-  return (
-    <>
-      <div>
-        <h1>Profile</h1>
-        <p>Name: {user.given_name}</p>
-        <p>Email: {user.email}</p>
-      </div>
-      <LogoutLink>Logout</LogoutLink>
-    </>
-  );
+  if (userData?.role == "GUIDE") return <h1>guide profile</h1>;
+
+  if (userData?.role == "TOURIST") return <h1>Tourist profile </h1>;
+
+  if (userData?.role == "VENDOR") return <h1>Vendor Profile</h1>;
+
+  return <>Admin Profile</>;
 };
 
 export default Profile;
