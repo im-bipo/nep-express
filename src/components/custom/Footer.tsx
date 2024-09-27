47a2b71148727c6b7de634ba2a3ed355885ea982
Pratikaryal23 <aryalpartik057@gmail.com>
2024-09-27 17:39:48 +0545

    ADD: Footer Section

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
+import esewa from "/public/Esewa_logo.webp";
+import fonepay from "/public/fonepay.png";
+import khalti from "/public/khalti.png";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-11/12 mx-auto py-10 gap-8">
+        <div className="flex flex-col gap-5">
+          <h1 className="text-lg lg:text-xl font-semibold text-secondary">
+            Connect With Us
+          </h1>
+          <div className="flex gap-4">
+            <FaFacebook className="text-lg lg:text-xl font-medium text-primary" />
+            <FaInstagram className="text-lg lg:text-xl font-medium text-primary" />
+            <FaYoutube className="text-lg lg:text-xl font-medium text-primary" />
+            <FaWhatsapp className="text-lg lg:text-xl font-medium text-primary" />
+            <FaTwitter className="text-lg lg:text-xl font-medium text-primary" />
+          </div>
+        </div>
 
-export default Footer
+        <div className="flex flex-col gap-5">
+          <h1 className="text-lg lg:text-xl font-semibold text-secondary">
+            About Nepexpress
+          </h1>
+          <div className="text-gray-400 flex flex-col gap-3">
+            <p>About Us</p>
+            <p>Blog</p>
+            <p>Contact us</p>
+            <p>Terms of Condition</p>
+            <p>Awards & Recognizations</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <h1 className="text-lg lg:text-xl font-semibold text-secondary">
+            Partner With Nepexpress
+          </h1>
+          <div className="text-gray-400 flex flex-col gap-3">
+            <p>Register as Guide</p>
+            <p>Add Your Product</p>
+          </div>
+          <h1 className="text-lg lg:text-xl font-semibold text-secondary">
+            Help & Support
+          </h1>
+          <div className="text-gray-400 flex flex-col gap-3">
+            <p>Phone: 9845455112</p>
+            <p>Email: info@nepexpress@gmail.com</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <h1 className="text-lg lg:text-xl font-semibold text-secondary">
+            Payment Method
+          </h1>
+          <div className="flex flex-col sm:flex-row gap-6">
+            <Image
+              src={khalti}
+              alt="Khalti Logo"
+              width={500}
+              height={500}
+              className="w-44 lg:w-60"
+            />
+            <div className="flex flex-col gap-4">
+              <Image
+                src={esewa}
+                alt="Esewa Logo"
+                width={500}
+                height={500}
+                className="w-36 lg:w-44"
+              />
+              <Image
+                src={fonepay}
+                alt="FonePay Logo"
+                width={500}
+                height={500}
+                className="w-36 lg:w-44"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
