b3dca9f071c4452335e90e0e1629a3235d1300dd
Pratikaryal23 <aryalpartik057@gmail.com>
2024-09-27 15:57:57 +0545

    ADD: Lander Sections

--- src/components/custom/HeroLander.tsx
+++ src/components/custom/HeroLander.tsx
@@ -4,11 +4,12 @@ import images from '/public/image 47 (1).png'
 import image1 from "/public/Rectangle 96.png";
 import image2 from "/public/image 48.png";
 import image3 from "/public/Rectangle 97.png";
-import { Button } from "./ui/button";
-// import Placerecommandate from "./Placerecommandate";
-// import Merchant from "./Merchant";
-// import Happy from "./Happy";
-// import Subscribe from "./Subscribe";
+import { Button } from "../ui/button";
+import Placerecozmmandate from "./Placerecozmmandate";
+import Happy from "./Happy";
+import Merchant from "./Merchant";
+import Subscribe from "./Subscribe";
+
 
 const HeroLander = () => {
   return (
@@ -17,33 +18,36 @@ const HeroLander = () => {
      
       <div className="flex flex-col gap-10 ">
         <div className="bg-gray-200 ">
-          <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 pt-20 ">
+          <div className=" grid grid-cols-1 md:grid-cols-2 gap-8 pt-20 ">
             <div className="flex flex-col gap-12">
-              <h1 className="text-xl font-medium font-serif">
+              <h1 className="  w-11/12 mx-auto  text-xl font-medium font-serif">
                 Welcome to <span className="text-primary">Nep</span>
                 <span className="text-secondary">express...</span>
               </h1>
-              <p className="text-md text-textcolor">
+              <p className="  w-11/12 mx-auto  text-md text-textcolor">
                 Discover the enchanting beauty and rich culture of Nepal with
                 Nepexpress, your ultimate destination for authentic Nepalese
                 experiences. Embrace the magic of this captivating nation with
                 Nepexpress and let Nepal's beauty and charm unfold before your
                 eyes.
               </p>
-              <div className="flex flex-col items-start gap-8">
+              <div className="flex  flex-col items-start gap-8">
+                <div className="w-11/12 mx-auto">
               <Button>Explore</Button>
-                <div className="w-full md:w-auto">
+              </div>
+                <div className="w-full md:w-full">
                   <Image
                     src={images}
                     alt="landing image1"
                     width={400}
                     height={400}
+                    className="-my-14"
                   />
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row gap-9">
+            <div className="  w-11/12 mx-auto flex flex-col md:flex-row gap-9">
               <div>
                 <Image src={image1} alt="image1" width={400} height={400} />
               </div>
@@ -67,18 +71,18 @@ const HeroLander = () => {
           </div>
          
         </div>
-        {/* <div>
-          <Placerecommandate />
+        <div>
+          <Placerecozmmandate />
         </div>
         <div>
-          <Merchant />
+          <Merchant/>
         </div>
         <div>
           <Happy/>
         </div>
         <div>
           <Subscribe/>
-        </div> */}
+        </div>
       </div>
       
     </>
---
+++ src/components/custom/Placerecozmmandate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Placerecozmmandate = () => {
+  return (
+    <div>Placerecozmmandate</div>
+  )
+}
+
+export default Placerecozmmandate
